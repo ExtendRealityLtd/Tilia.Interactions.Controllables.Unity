4ce83187fd111b8c419b42377b702f8b565411e1
extendreality <hello@extendreality.io>
2020-02-26 14:49:39 +0000

    feat(structure): port controllable prefabs
    The controllable prefabs from VRTK.Prefabs have been ported over to
    their own Tilia repo and changed slightly to make new prefabs based on
    the new Tilia Interactable prefab.
    
    The basic installation guide has been added as well.

---
+++ Runtime/SharedResources/Scripts/LinearDriver/LinearDriveFacade.cs
@@ -0,0 +1,55 @@
+﻿namespace Tilia.Interactions.Controllables.LinearDriver
+{
+    using Malimbe.MemberChangeMethod;
+    using Malimbe.PropertySerializationAttribute;
+    using Malimbe.XmlDocumentationAttribute;
+    using Tilia.Interactions.Controllables.Driver;
+    using UnityEngine;
+    using Zinnia.Data.Attribute;
+
+    /// <summary>
+    /// The public interface into any Linear Drive prefab.
+    /// </summary>
+    public class LinearDriveFacade : DriveFacade<LinearDrive, LinearDriveFacade>
+    {
+        #region Limit Settings
+        /// <summary>
+        /// The world space limit of the drive direction along the specified axis.
+        /// </summary>
+        [Serialized]
+        [field: Header("Limit Settings"), DocumentedByXml]
+        public float DriveLimit { get; set; } = 1f;
+        #endregion
+
+        #region Gizmo Settings
+        /// <summary>
+        /// The size of the gizmo cube to draw at the limits of the drive.
+        /// </summary>
+        [Serialized]
+        [field: Header("Gizmo Settings"), DocumentedByXml, Restricted(RestrictedAttribute.Restrictions.Muted)]
+        public Vector3 GizmoCubeSize { get; set; } = Vector3.one * 0.015f;
+        #endregion
+
+        protected virtual void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.matrix = transform.localToWorldMatrix;
+            Vector3 origin = Vector3.zero;
+            Vector3 direction = DriveAxis.GetAxisDirection(true) * (DriveLimit * 0.5f);
+            Vector3 from = origin - direction;
+            Vector3 to = origin + direction;
+            Gizmos.DrawLine(from, to);
+            Gizmos.DrawCube(from, GizmoCubeSize);
+            Gizmos.DrawCube(to, GizmoCubeSize);
+        }
+
+        /// <summary>
+        /// Called after <see cref="DriveLimit"/> has been changed.
+        /// </summary>
+        [CalledAfterChangeOf(nameof(DriveLimit))]
+        protected virtual void OnAfterDriveLimitChange()
+        {
+            Drive.SetUp();
+        }
+    }
+}
