90b78aab5e4c2c37cd318bd279f2f1f269157d03
Harvey Ball <me@harveyball.com>
2022-03-03 12:23:14 +0000

    feat(Editor): add controllable creator window
    The new Controllable Creator window makes it easier to turn an existing
    scene mesh into a controllable by automatically adding the selected
    prefab and nesting the mesh in the appropriate location.

---
+++ Editor/Interactables/ControllableCreatorEditorWindow.cs
@@ -0,0 +1,133 @@
+﻿namespace Tilia.Interactions.Controllables.Interactables
+{
+    using Tilia.Interactions.Controllables.AngularDriver;
+    using Tilia.Interactions.Controllables.LinearDriver;
+    using Tilia.Interactions.Interactables.Interactables;
+    using UnityEditor;
+    using UnityEngine;
+
+    [InitializeOnLoad]
+    public class ControllableCreatorEditorWindow : EditorWindow
+    {
+        private const string windowTitle = "Controllable Creator";
+        private const string angularJointAsset = "Interactions.AngularJointDrive";
+        private const string linearJointAsset = "Interactions.LinearJointDrive";
+        private const string angularTransformAsset = "Interactions.AngularTransformDrive";
+        private const string linearTransformAsset = "Interactions.LinearTransformDrive";
+        private const string assetSuffix = ".prefab";
+        private static EditorWindow promptWindow;
+        private Vector2 scrollPosition;
+        private GameObject angularJointPrefab;
+        private GameObject linearJointPrefab;
+        private GameObject angularTransformPrefab;
+        private GameObject linearTransformPrefab;
+        private string[] prefabTypes = new string[]
+        {
+            "Linear Joint Drive",
+            "Linear Transform Drive",
+            "Angular Joint Drive",
+            "Angular Transform Drive"
+        };
+        private int prefabTypeIndex = 0;
+
+        public void OnGUI()
+        {
+            using (GUILayout.ScrollViewScope scrollViewScope = new GUILayout.ScrollViewScope(scrollPosition))
+            {
+                scrollPosition = scrollViewScope.scrollPosition;
+                GUILayout.Label("Controllable Creator", EditorStyles.boldLabel);
+
+                prefabTypeIndex = EditorGUILayout.Popup(prefabTypeIndex, prefabTypes);
+
+                if (GUILayout.Button("Convert"))
+                {
+                    switch (prefabTypeIndex)
+                    {
+                        case 0:
+                            ProcessSelectedGameObjects<LinearJointDrive>(linearJointPrefab);
+                            break;
+                        case 1:
+                            ProcessSelectedGameObjects<LinearTransformDrive>(linearTransformPrefab);
+                            break;
+                        case 2:
+                            ProcessSelectedGameObjects<AngularJointDrive>(angularJointPrefab);
+                            break;
+                        case 3:
+                            ProcessSelectedGameObjects<AngularTransformDrive>(angularTransformPrefab);
+                            break;
+                    }
+
+                }
+            }
+        }
+
+        protected virtual void OnEnable()
+        {
+            SetAsset(angularJointAsset, ref angularJointPrefab);
+            SetAsset(linearJointAsset, ref linearJointPrefab);
+            SetAsset(angularTransformAsset, ref angularTransformPrefab);
+            SetAsset(linearTransformAsset, ref linearTransformPrefab);
+        }
+
+        protected virtual void SetAsset(string assetName, ref GameObject prefab)
+        {
+            foreach (string assetGUID in AssetDatabase.FindAssets(assetName))
+            {
+                string assetPath = AssetDatabase.GUIDToAssetPath(assetGUID);
+                if (assetPath.Contains(assetName + assetSuffix))
+                {
+                    prefab = AssetDatabase.LoadAssetAtPath<GameObject>(assetPath);
+                }
+            }
+        }
+
+        protected virtual void ProcessSelectedGameObjects<DriveType>(GameObject prefabToUse)
+        {
+            foreach (Transform selectedTransform in Selection.transforms)
+            {
+                ConvertSelectedGameObject<DriveType>(selectedTransform.gameObject, prefabToUse);
+            }
+        }
+
+        protected virtual void ConvertSelectedGameObject<DriveType>(GameObject objectToConvert, GameObject prefabToUse)
+        {
+            DriveType interactable = objectToConvert.GetComponentInParent<DriveType>();
+            if (interactable == null)
+            {
+                CreateInteractable(objectToConvert, prefabToUse);
+            }
+        }
+
+        protected virtual void CreateInteractable(GameObject objectToWrap, GameObject prefabToUse)
+        {
+            int siblingIndex = objectToWrap.transform.GetSiblingIndex();
+            GameObject newInteractable = (GameObject)PrefabUtility.InstantiatePrefab(prefabToUse);
+            newInteractable.name += "_" + objectToWrap.name;
+            InteractableFacade facade = newInteractable.GetComponentInChildren<InteractableFacade>();
+
+            newInteractable.transform.SetParent(objectToWrap.transform.parent);
+            newInteractable.transform.localPosition = objectToWrap.transform.localPosition;
+            newInteractable.transform.localRotation = objectToWrap.transform.localRotation;
+            newInteractable.transform.localScale = objectToWrap.transform.localScale;
+
+            foreach (MeshRenderer defaultMeshes in facade.Configuration.MeshContainer.GetComponentsInChildren<MeshRenderer>())
+            {
+                defaultMeshes.gameObject.SetActive(false);
+            }
+
+            objectToWrap.transform.SetParent(facade.Configuration.MeshContainer.transform);
+            objectToWrap.transform.localPosition = Vector3.zero;
+            objectToWrap.transform.localRotation = Quaternion.identity;
+            objectToWrap.transform.localScale = Vector3.one;
+
+            newInteractable.transform.SetSiblingIndex(siblingIndex);
+        }
+
+        [MenuItem("Window/Tilia/Interactions/" + windowTitle)]
+        private static void ShowWindow()
+        {
+            promptWindow = EditorWindow.GetWindow(typeof(ControllableCreatorEditorWindow));
+            promptWindow.titleContent = new GUIContent(windowTitle);
+        }
+    }
+}
