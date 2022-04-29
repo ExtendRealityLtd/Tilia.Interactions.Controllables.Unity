6efd2c8a00e65bf907916a81b0c768235f78ef05
Harvey Ball <me@harveyball.com>
2022-04-29 09:30:48 +0100

    feat(LinearDriveFacade): editor button to align to initial target value
    A new custom editor for the LinearDriveFacade has been added that just
    adds a button to the bottom of the component inspector that when
    clicked will align the linear drive component in the editor scene to
    the position that the initial target value is set to.
    
    This makes it easier to line up the drive object in the editor as it
    can be shown at the various stages of its travel.

---
+++ Editor/Interactables/LinearDriveFacadeEditor.cs
@@ -0,0 +1,79 @@
+﻿namespace Tilia.Interactions.Controllables.LinearDriver
+{
+    using UnityEditor;
+    using UnityEngine;
+    using Zinnia.Utility;
+
+    [CustomEditor(typeof(LinearDriveFacade), true)]
+    public class LinearDriveFacadeEditor : ZinniaInspector
+    {
+        private const string buttonText = "Align To Initial Target Value";
+        private LinearDriveFacade facade;
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            EditorGUILayout.BeginHorizontal("GroupBox");
+            GUILayout.FlexibleSpace();
+            if (GUILayout.Button(buttonText))
+            {
+                ChooseButtonLogic();
+            }
+            GUILayout.FlexibleSpace();
+            EditorGUILayout.EndHorizontal();
+        }
+
+        protected virtual void OnEnable()
+        {
+            facade = (LinearDriveFacade)serializedObject.targetObject;
+        }
+
+        protected virtual void ChooseButtonLogic()
+        {
+            Vector3 newPosition = GetNewPosition();
+            if (facade.Drive.GetType() == typeof(LinearTransformDrive))
+            {
+                AlignTransformToInitialTargetValue(newPosition);
+            }
+            else if (facade.Drive.GetType() == typeof(LinearJointDrive))
+            {
+                AlignJointToInitialTargetValue(newPosition);
+            }
+        }
+
+        protected virtual void AlignJointToInitialTargetValue(Vector3 newPosition)
+        {
+            LinearJointDrive drive = (LinearJointDrive)facade.Drive;
+            drive.Joint.transform.localPosition = newPosition;
+        }
+
+        protected virtual void AlignTransformToInitialTargetValue(Vector3 newPosition)
+        {
+            LinearTransformDrive drive = (LinearTransformDrive)facade.Drive;
+            drive.Interactable.transform.localPosition = newPosition;
+        }
+
+        protected virtual float CalculatePosition()
+        {
+            float halfLimit = facade.DriveLimit * 0.5f;
+            return Mathf.Lerp(-halfLimit, halfLimit, facade.InitialTargetValue);
+        }
+
+        protected virtual Vector3 GetNewPosition()
+        {
+            float position = CalculatePosition();
+            switch (facade.DriveAxis)
+            {
+                case Driver.DriveAxis.Axis.XAxis:
+                    return Vector3.right * position;
+                case Driver.DriveAxis.Axis.YAxis:
+                    return Vector3.up * position;
+                case Driver.DriveAxis.Axis.ZAxis:
+                    return Vector3.forward * position;
+            }
+
+            return Vector3.zero;
+        }
+    }
+}
